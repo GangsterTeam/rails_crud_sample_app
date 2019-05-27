ada782d7c94777c171d4766c2cafc0523c3816c2
BK <baladkb@gmail.com>
2019-05-27 15:33:10 +0530

    list all saved items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Sample.create(name: 'BK')
+Sample.create(name: 'Balaji')
+Sample.create(name: 'SK')
