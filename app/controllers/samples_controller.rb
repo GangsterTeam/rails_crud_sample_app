64c99b579c50a8ecb3966115cd4acdc67eac38bb
BK <baladkb@gmail.com>
2019-05-27 15:44:50 +0530

    create new record

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -8,6 +8,18 @@ class SamplesController < ApplicationController
   end
 
   def new
+  	@sample = Sample.new
+  end
+
+  def create
+    sample = Sample.create(sample_params)
+	redirect_to samples_path
+  end
+
+private
+
+  def sample_params
+    params.require(:sample).permit(:name)
   end
 
   def edit
